30c4613d061e5040f2b8c7666c75d635f601b2b7
Phuc <Phuc@PHUC-PC>
2020-09-21 12:45:43 +0700

    Add project files.

---
+++ ECommerce.Data/Models/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Data.Models
+{
+   public  class Users
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public  string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string DBO { get; set; }
+        public string FullName { get; set; }
+        
+
+
+
+
+
+
+
+    }
+}
